0aa0330bf8f7e776036267051a96c2b24b460937
CameronJMurphy <50137701+CameronJMurphy@users.noreply.github.com>
2025-05-14 11:20:43 +1000

    CharacterMovement implemented

--- Cert_3_Collab/Assets/Scripts/CameraControl.cs
+++ Cert_3_Collab/Assets/Scripts/CameraControl.cs
@@ -8,29 +8,24 @@ public class CameraControl : MonoBehaviour
     private Transform target;
     private Vector3 velocity;
     private Vector3 desiredPos;
-    private string[] orks = { "greg", "billy", "gorguz", "fred" };
-    
-    public List<string> tasks;
+    public float rotationSpeed = 30;
     void Start()
     {
-       target = GameObject.FindGameObjectWithTag("Player").transform;
-       tasks = new List<string>();
+       target = GameObject.FindGameObjectWithTag("CameraTarget").transform;
     }
 
     void Update()
     {
         Move();
-        Debug.Log(orks.Length);
-        for(int i = 0; i < orks.Length; i++)
-        {
-            Debug.Log(orks[i]);
-            tasks.Add(orks[i]);
-        }
+        float y_mouse_axis = Input.GetAxisRaw("Mouse Y");
+        transform.Rotate(Vector3.right,-y_mouse_axis * rotationSpeed * Time.deltaTime );
     }
 
     private void Move()
     {
         desiredPos = target.position;
         transform.position = Vector3.SmoothDamp(transform.position,desiredPos,ref velocity,dampTime);
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.LookAt(player.position);
     }
 }
--- Cert_3_Collab/Assets/Scripts/PlayerMovement.cs
+++ Cert_3_Collab/Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,26 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
     public float force = 5.0f;
     public Vector3 dir = Vector3.right;
+    CharacterController controller;
+    public float speed = 100;
+    public float rotationSpeed = 10;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        rb.AddForce(dir * force);
+        //rb.AddForce(dir * force);
+        Vector3 dir = Vector3.zero;
+        float h_axis = Input.GetAxisRaw("Horizontal");
+        float v_axis = Input.GetAxisRaw("Vertical");
+        dir = (v_axis * transform.forward  + h_axis * transform.right).normalized * Time.deltaTime * speed;
+        Debug.Log(dir);
+        controller.Move(dir);
+
+        float x_mouse_axis = Input.GetAxisRaw("Mouse X");
+        transform.Rotate(Vector3.up, x_mouse_axis * Time.deltaTime * rotationSpeed);
     }
 }
